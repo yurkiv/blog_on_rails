8b22210b1a1dc9bb3da2a3255a232037ce5035bc
Misha Yurkiv <jurkiw.misha@gmail.com>
2015-10-02 13:23:17 +0300

    updated user test(added abilities)

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require "cancan/matchers"
 
 RSpec.describe User, type: :model do
   before { @user = User.new(email: "user@example.com", password: "12345678") }
@@ -36,5 +37,66 @@ RSpec.describe User, type: :model do
     it { should_not be_valid }
   end
 
+  describe 'Abilities' do
 
+    let(:user) { @user }
+    subject { Ability.new(user) }
+
+    context "a guest user" do
+      it "cannot create article" do
+        expect(subject).to_not be_able_to(:create, Article)
+      end
+
+      it "cannot update article" do
+        expect(subject).to_not be_able_to(:update, Article)
+      end
+
+      it "cannot destroy article" do
+        expect(subject).to_not be_able_to(:create, Article)
+      end
+
+      it "can read article" do
+        expect(subject).to be_able_to(:read, Article)
+      end
+
+    end
+
+    context "a signed user" do
+      let(:user) { @user.save }
+      it "can create article" do
+        expect(subject).to_not be_able_to(:create, Article)
+      end
+
+      it "can update article" do
+        expect(subject).to_not be_able_to(:update, Article)
+      end
+
+      it "can destroy article" do
+        expect(subject).to_not be_able_to(:create, Article)
+      end
+
+      it "can read article" do
+        expect(subject).to be_able_to(:read, Article)
+      end
+
+    end
+
+      # context 'guest' do
+      #   let(:user) { @user }
+      #
+      #   it "cannot create article" do
+      #     ability = Ability.new(user)
+      #     it{ should be_able_to(:manage, Article.new) }
+      #     # assert ability.can?(:create, Article.new)
+      #   end
+      #
+      #
+      #
+      #   #
+      #   # it 'can read shares' do
+      #   #   ability = Ability.new(user)
+      #   #   assert ability.can?(:read, :share)
+      #   # end
+      # end
+    end
 end
