a28116225d1e9f6197bd561653e81b8fc1ef3bfe
Misha Yurkiv <jurkiw.misha@gmail.com>
2015-10-07 09:51:08 +0300

    updated basic admin panel

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,54 @@
+class Admin::UsersController < ApplicationController
+  load_and_authorize_resource
+
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:notice] = "Successfully created User."
+      redirect_to users_path
+    else
+      render :action => 'new'
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+      @user = User.find(params[:id])
+      params[:user].delete(:password) if params[:user][:password].blank?
+      params[:user].delete(:password_confirmation) if params[:user][:password].blank? and params[:user][:password_confirmation].blank?
+      if @user.update_attributes(user_params)
+        @user.roles.clear
+        @user.add_role params[:user][:roles]
+        flash[:notice] = "Successfully updated User."
+        redirect_to users_path
+      else
+        render :action => 'edit'
+      end
+    end
+
+  def destroy
+    @user = User.find(params[:id])
+    if @user.destroy
+      flash[:notice] = "Successfully deleted User."
+      redirect_to root_path
+    end
+  end
+
+  def user_params
+  	if can? :manage, User
+  		params.require(:user).permit(:email, :password, :password_confirmation)
+  	end
+  end
+
+end
